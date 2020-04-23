933aee423a91d34eb6a2a51276317534d433e65a
reus <reusee@gmail.com>
2020-04-23 12:26:09 +0800

    archive inactive repos

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/reusee/github-cleanup
+
+go 1.14
+
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/reusee/e/v2 v2.3.3
+	github.com/reusee/toml v0.4.0
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/google/go-github/github"
+	"github.com/reusee/e/v2"
+	"github.com/reusee/toml"
+	"golang.org/x/oauth2"
+)
+
+var (
+	me     = e.Default.WithStack()
+	ce, he = e.New(me)
+	pt     = fmt.Printf
+)
+
+func main() {
+
+	// config
+	var oauthConfig struct {
+		Token string // oauth2 access token, create one at https://github.com/settings/tokens
+	}
+	dir, err := os.UserConfigDir()
+	ce(err)
+	content, err := ioutil.ReadFile(filepath.Join(dir, "github.conf.toml"))
+	ce(err)
+	ce(toml.Unmarshal(content, &oauthConfig))
+
+	// client
+	ctx := context.Background()
+	client := github.NewClient(
+		oauth2.NewClient(
+			ctx,
+			oauth2.StaticTokenSource(
+				&oauth2.Token{
+					AccessToken: oauthConfig.Token,
+				},
+			),
+		),
+	)
+
+	// archive inactive repos
+	page := 0
+	for {
+		repos, _, err := client.Repositories.List(ctx, "", &github.RepositoryListOptions{
+			ListOptions: github.ListOptions{
+				Page:    page,
+				PerPage: 100,
+			},
+		})
+		ce(err)
+
+		for _, repo := range repos {
+			if time.Since(repo.UpdatedAt.Time) < time.Hour*24*365*1 {
+				continue
+			}
+			if *repo.Archived {
+				continue
+			}
+			pt(
+				"%s\n\t%v\n\t%v\n",
+				*repo.Name,
+				*repo.UpdatedAt,
+				*repo.HTMLURL,
+			)
+			t := true
+			repo.Archived = &t
+			_, _, err := client.Repositories.Edit(ctx, *repo.Owner.Login, *repo.Name, repo)
+			ce(err)
+		}
+
+		if len(repos) == 0 {
+			break
+		}
+		page++
+	}
+
+}
